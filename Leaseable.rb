61b7dffe8587f6a79742d5c6043d6a6ac2fc4761
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2013-05-30 18:29:58 -0300

    Monitor com lista de objetos leaseable

--- Leaseable.rb
+++ Leaseable.rb
@@ -1,10 +1,11 @@
 module Leaseable
-  def startLease(monitor, time)
+  def start_lease(time)
     @startTime = Time.now
-
+    @time = time
+=begin
     Thread.new do
       loop {
-        if Time.now - @startTime >= time
+        if Time.now - @startTime >= @time
           monitor.leaseExpired(self)
           break
         end
@@ -12,6 +13,11 @@ module Leaseable
         sleep 0.3
       }
     end
+=end
+  end
+
+  def expired_lease?
+    return Time.now - @startTime >= @time
   end
 
   def renewLease
@@ -20,8 +26,33 @@ module Leaseable
 end
 
 class Monitor
-  def leaseExpired(obj)
+  def initialize
+    @monitored_objects = []
+  end
+
+  def start_verify_leases
+    Thread.new do
+      loop {
+        
+        @monitored_objects.each do |obj|
+          if obj.expired_lease?
+            lease_expired(obj)
+          end
+        end
+
+        sleep 0.3
+      }
+    end
+  end
+
+  def conceed_lease(obj, lease_time)
+    obj.start_lease(lease_time)
+    @monitored_objects << obj
+  end
+
+  def lease_expired(obj)
     puts "Lease ended: " + obj.inspect
+    @monitored_objects.delete obj
   end
 end
 
@@ -32,7 +63,14 @@ end
 monitor = Monitor.new
 
 foo = Foo.new
-foo.startLease(monitor, 1)
+foo2 = Foo.new
+foo3 = Foo.new
+
+monitor.conceed_lease foo, 1
+monitor.conceed_lease foo2, 2
+monitor.conceed_lease foo3, 3
+
+monitor.start_verify_leases
 
 loop {
 }
