d36173a27b57d01186052f0ec554e66c1c169585
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2013-05-29 21:51:17 -0300

    Iniciando a config das rotas

---
+++ library.rb
@@ -0,0 +1,8 @@
+class Library
+  def get_potato(potato)
+    return "Metodo potato da classe Library escrevendo: " + hue
+  end
+
+  def post_potato(potato)
+  end
+end
--- server_request_handler.rb
+++ server_request_handler.rb
@@ -1,71 +1,56 @@
 require 'webrick'  
 require 'time'
 
-class ServerRequestHandler 
+class ServerRequestHandler
   include WEBrick
+
+  def initialize
+  end
 
   def start
     root = File.expand_path '~/public_html'
     @server = WEBrick::HTTPServer.new(:Port => 8000)
     
-    @server.mount "/questions", eval("WebForm")
+    @server.mount "/", Router
+
+    register_paths
 
     trap("INT"){ @server.shutdown }
 
     @server.start
   end
 
-  def listen
-  end
-
-  def handle_request(client)
-
-    Thread.new do
-
-      line = client.gets
-
-      puts line
-
-      resp = "<html>#{Time.now.httpdate()}</html>"
-
-      headers = ["HTTP/1.1 200 OK",
-                 "Date: #{Time.now.httpdate()}",
-                 "Server: Ruby",
-                 "Content-Type: text/html; charset=iso-8859-1",
-                 "Content-Length: #{resp.length}\r\n\r\n"].join("\r\n")
-
-      puts "========================"
-
-      client.puts headers
-
-      client.puts resp
-
-      client.close
+  def register_paths
+    File.open("config/routes.config", "r").each(sep="\n") do |file|
+      puts file
     end
   end
-end
 
-class WebForm < WEBrick::HTTPServlet::AbstractServlet
   def do_GET(request, response)
-    status, content_type, body = print_questions(request)
-
-    response.status = status
-    response['Content-Type'] = content_type
-    response.body = body
+    puts request.inspect
+    puts response.inspect
   end
 
-  def print_questions(request)
-    html = "<h1>uehuehuehueheuheueh</h1>"
-    return 200, "text/html", html
+  def do_POST(request, response)
+    puts request.inspect
+    puts response.inspect
   end
 end
 
+class Router < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    puts request.inspect
+    puts response.inspect
+  end
 
+  def do_POST(request, response)
+    puts request.inspect
+    puts response.inspect
+  end
+end
 
 if $0 == __FILE__ then
-
   srh = ServerRequestHandler.new
   srh.start
   srh.listen
-
 end
