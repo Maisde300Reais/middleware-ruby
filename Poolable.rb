73d67f03457ad9086882fd0a21722433be5e9641
roger <luizrogeriocn@gmail.com>
2013-05-31 06:48:09 -0300

    usando Poolable.rb de rascunho por enquanto

--- Poolable.rb
+++ Poolable.rb
@@ -2,22 +2,26 @@ module Poolable
 
 	#metodo que retorna uma instancia da pool
 	#importante: só deve existir uma instancia da pool (singleton pattern)
-	def get_pool_instance 
+	def get_instance 
 	end
 
 	#metodo para definir o tamanho maximo que a pool deve ter
 	#o tamanho influenciará nas decisoes a serem tomadas por um futuro pool manager
-	def set_max_pool_size
-	end
-
-	#metodo para retirar um objeto da pool
-	#objeto retirado da pool entrará num estado de "trabalhando"
-	def acquire_obj_from_pool
+	def set_size
 	end
 
 	#metodo para colocar um objeto de volta na pool
 	#objeto colocado na pool entrará num estado de "disponivel"
 	def release_obj
+	end
+
+	#**********************************************************#
+	#***métodos abaixo ainda não foram implementados na pool***#
+	#**********************************************************#
+
+	#metodo para retirar um objeto da pool
+	#objeto retirado da pool entrará num estado de "trabalhando"
+	def acquire_obj
 	end
 
 	#metodo para criar novos objetos na pool
