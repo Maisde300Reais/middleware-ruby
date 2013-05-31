e6ef32fc3adb6a46d3e36864561386f4e718516f
roger <luizrogeriocn@gmail.com>
2013-05-31 04:37:57 -0300

    modificando leaseable

--- Leaseable.rb
+++ Leaseable.rb
@@ -1,64 +1,17 @@
 module Leaseable
-  def start_lease(time)
-    @startTime = Time.now
-    @time = time
+  def start_lease(time_to_live)
+    @start_time = Time.now
+    @time_to_live = time_to_live
+    @renewals = 0
   end
 
   def expired_lease?
-    return Time.now - @startTime >= @time
+    return Time.now - @start_time >= @time_to_live
   end
 
-  def renewLease
-    @startTime = Time.now
+  def renew_lease
+    @start_time = Time.now
+    @renewals = @renewals + 1
   end
+
 end
-
-class Monitor
-  def initialize
-    @monitored_objects = []
-  end
-
-  def start_verify_leases
-    Thread.new do
-      loop {
-        
-        @monitored_objects.each do |obj|
-          if obj.expired_lease?
-            lease_expired(obj)
-          end
-        end
-
-        sleep 0.3
-      }
-    end
-  end
-
-  def grant_lease(obj, lease_time)
-    obj.start_lease(lease_time)
-    @monitored_objects << obj
-  end
-
-  def lease_expired(obj)
-    puts "Lease ended: " + obj.inspect
-    @monitored_objects.delete obj
-  end
-end
-
-class Foo
-  include Leaseable
-end
-
-monitor = Monitor.new
-
-foo = Foo.new
-foo2 = Foo.new
-foo3 = Foo.new
-
-monitor.grant_lease foo, 1
-monitor.grant_lease foo2, 2
-monitor.grant_lease foo3, 3
-
-monitor.start_verify_leases
-
-loop {
-}
