83225c830d10d08081aeee464a7aa588ab600dd8
roger <luizrogeriocn@gmail.com>
2013-05-31 04:39:06 -0300

    Poolable adicionado

--- Poolable.rb
+++ Poolable.rb
@@ -1,2 +1,20 @@
 module Poolable
+
+	def get_pool_instance
+	end
+
+	def set_max_pool_size
+	end
+
+	def acquire_obj_from_pool
+	end
+
+	def create_new_obj
+	end
+
+	def release_obj
+	end
+
+
+
 end
