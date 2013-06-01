cc7126d1625b89d672cf10840834fb144a9f6d12
roger <luizrogeriocn@gmail.com>
2013-06-01 00:43:20 -0300

    Pool com alguns metodos implementados

--- Pool.rb
+++ Pool.rb
@@ -17,28 +17,45 @@ class Pool
 		@size = new_size
 	end
 
-	def aquire
+	def acquire
 		if @available_objects.empty?
-			puts "pool vazia -- implementar isso"
+			puts "pool vazia -- criar novo objeto?!"
 		else
-			puts "não tá vazia -- implementar isso"
+			temporary_object = @available_objects[0]
+			@available_objects.shift
+			@working_objects << temporary_object
+			return temporary_object
 		end
 	end
 
 	def release(object)
+		temporary_object = @working_objects.delete(object)
+		@available_objects << temporary_object
+	end
+
+	def add_object(object)
 		@available_objects << object
 	end
 
 	private_class_method :new
 end
 
-#Pool implementando singleton com sucesso (só existirá uma instancia da pool) =D
 #abaixo é só o que usei pra testar
+class Foo
+	attr_accessor :id
+	def initialize(n)
+    @id = n
+  end
+end
+obj1 = Foo.new("1")
+obj2 = Foo.new("2")
 
-poo1 = Pool.get_instance
-puts poo1.size
-poo1.set_size(7)
-puts poo1.size
-poo2 = Pool.get_instance
-puts poo2.size
-poo2.aquire
+pool = Pool.get_instance
+pool.add_object(obj1)
+pool.add_object(obj2)
+
+user1 = pool.acquire
+user2 = pool.acquire
+puts user1.id
+puts user2.id
+pool.release(user2)
