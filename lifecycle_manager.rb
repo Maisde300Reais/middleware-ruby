57df29c5a875039802f817b5c5c947b4ba4b1a19
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-06-09 14:18:31 -0300

    removendo um server inutil

--- lifecycle_manager.rb
+++ lifecycle_manager.rb
@@ -1,40 +1,86 @@
-require_relative 'invocation'
+require_relative 'Pool'
 
-class Lifecycle_Manager
+class Lifecycle_manager
+	attr_accessor :hash
 
-	# vários LMs podem ser definidos pra cada estratégia 
 	def initialize()
-
+		@hash_register = {}
+		@passivated_objects = {}
+		@pool = Pool.get_instance()
 	end
 
-
-	# invoker informa que precisa de um objeto
-		# inicia um tempo e ativa um obj remoto
-	def invocation_arrived() # objID, ...
-
+	#set strategy for each class here
+	def register_object_as(class_name, strategy)
+		@hash[class_name]=strategy
 	end
 
-	# Depois que o invoker recebe o obj remoto avisa
-		# parar o contador do invocation_arrived
-	def invocation_done() # objID, ...
+	def pick_object(object, unique_id)
+		strategy = @hash_register[object.class]
 
+		case strategy
+		when "Lazy"
+			puts "It's Lazy, f*ck this sh*t, call it now!"
+
+		when "Passivation"
+			pick_persistable()
+
+		when "Poolable"
+			pick_poolable()
+
+		when "Leaseable"
+			pick_leaseable()
+
+		else
+			:ObjectClassNotRegistered
+
+		end
 	end
 
-	# ativar o obj remoto
-		# Permite que o invoker o invoke
-	def activate()
-
+	def pick_poolable()
+		puts "It's poolable, need to check if it is already at work or available"
 	end
 
-	# desativar o obj remoto
-		# definir se apaga, joga na pool, etc
-	def deactive()
-
+	def pick_persistable()
+		puts "It's Persistable, need to check if it is active or persisted"
 	end
 
-
-	# para objetos complexos [sem estado trivial]
-		# informa sobre futuros eventos do lifecycle
-	def lifecycle_callback()
-
+	def pick_leaseable()
+		puts "It's leaseable, need to check if its lease has expired, if not expired I will renew it!"
 	end
+	
+end
+
+class Foo
+	attr_accessor :id
+	def initialize(id)
+		@id = id
+	end
+end
+
+class Fuu
+	attr_accessor :id
+	def initialize(id)
+		@id = id
+	end
+end
+
+class Chutambo
+	attr_accessor :attack
+	def initialize(id)
+		@attack = attack
+	end
+end
+
+manager = Lifecycle_manager.new()
+
+rebeca = Foo.new(1)
+larissa = Fuu.new(2)
+igor = Chutambo.new("porrada")
+
+manager.register_object_as(larissa.class, "Passivation")
+manager.register_object_as(rebeca.class, "Lazy")
+manager.register_object_as(igor.class, "Poolable")
+
+manager.pick_object(larissa, igor.attack)
+manager.pick_object(rebeca, igor.attack)
+manager.pick_object(igor, igor.attack)
