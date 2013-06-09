58c8752dabf784eda8017fc2ba9a243d955a167b
luizrogeriocn <luizrogeriocn@gmail.com>
2013-06-09 16:10:00 -0300

    group_confg e lifecycle manager integrados

--- group_config.rb
+++ group_config.rb
@@ -1,12 +1,16 @@
-require 'singleton'
 
 class Group_config
-	include Singleton
 	
 	attr_accessor :registered
 
 	def initialize()
 		@registered = {}
+	end
+
+	@@instance = Group_config.new
+
+	def self.get_instance
+		return @@instance
 	end
 
 	def register_class_as(class_name, strategy)
@@ -16,5 +20,7 @@ class Group_config
 	def get_strategy(object)
 		@registered[object.class]
 	end
+
+	private_class_method :new
 
 end
