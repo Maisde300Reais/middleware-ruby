3944cb1e9e58c87eb2c74337010be6d90e911e7a
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-06-09 23:00:05 -0300

    App de rogério adicionada, mas nao testada

--- lifecycle_manager.rb
+++ lifecycle_manager.rb
@@ -7,10 +7,9 @@ class Lifecycle_manager
 
 	def initialize()
 		@remote_objects = {}
-		@persisted_objects = {}
 		@register = {}
 		@pool = Pool.instance
-		@persist = Passivation.new
+		@persist = Passivation.instance
 		@config = Group_config.instance
 	end
 
@@ -19,10 +18,10 @@ class Lifecycle_manager
     	#falta fazer a ação correspondente a criação desse obj
   	end
 
-  def get_remote_object(unique_id)
+  	def get_remote_object(unique_id)
 		object = @remote_objects[unique_id]
 		return pick_object(object, unique_id)
-  end
+  	end
 
 	def pick_object(object, unique_id)
 		strategy = @config.get_strategy(object)
@@ -30,10 +29,10 @@ class Lifecycle_manager
 		case strategy
 		
 		when "Passivation"
-			pick_persistable(unique_id)
+			@persist.pick_persistable(unique_id)
 
 		when "Poolable"
-			pick_poolable(unique_id)
+			@pool.pick(unique_id)
 
 		#a partir daqui, não sei se precisa de tratamento proprio..
 		when "Lazy"
@@ -44,33 +43,11 @@ class Lifecycle_manager
 
 		else
 			puts object.inspect
-			#ObjectClassNotRegistered -> Eager_acquisition
+			#ObjectClassNotRegistered -> Eager_acquisition <-
 
 		end
 	end
 
-	def pick_poolable(unique_id)
-		return @pool.pick(unique_id)
-	end
-
-	def pick_persistable(unique_id)
-
-		if !@persisted_objects.include?(unique_id)
-			puts "não foi colocado em disco, de onde retornar?"
-		else
-			object = @persisted_objects.delete(unique_id)
-			@persist.activate(object, unique_id)
-		end
-
-	end
-
-	def pick_leaseable()
-		puts "It's leaseable, need to check if its lease has expired, if not expired I will renew it!"
-	end
-
-	def pick_eager()
-		puts "Just gotta return the object from the lookup table!"
-	end
 	
 end
 
@@ -88,22 +65,16 @@ class Fuu
 	end
 end
 
+manager = Lifecycle_manager.new()
 
-def test
-	manager = Lifecycle_manager.new()
+rebeca = Foo.new(1)
+larissa = Fuu.new(2)
 
-	rebeca = Foo.new(1)
-	larissa = Fuu.new(2)
+manager.config.register_class_as(larissa.class, "Passivation")
+manager.config.register_class_as(rebeca.class, "Poolable")
 
-	manager.config.register_class_as(larissa.class, "Passivation")
-	manager.config.register_class_as(rebeca.class, "Poolable")
+manager.pool.add(rebeca, rebeca.id)
+manager.persist.passivate(larissa, larissa.id)
 
-	manager.pool.add(rebeca, rebeca.id)
-	manager.persist.passivate(larissa, larissa.id)
-	manager.persisted_objects[larissa.id]=larissa
-
-	puts manager.pick_object(rebeca, rebeca.id).inspect
-	puts manager.pick_object(larissa, larissa.id).inspect
-end
-
-test
+puts manager.pick_object(rebeca, rebeca.id).inspect
+puts manager.pick_object(larissa, larissa.id).inspect
