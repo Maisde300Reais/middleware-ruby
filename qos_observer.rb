346e9ac8c1879edc0529e13f9c4cd26657c9c1f3
SeuRAUL <kenny.is.inmortal@gmail.com>
2013-06-10 12:33:05 -0300

    qos_observer

---
+++ qos_observer.rb
@@ -0,0 +1,38 @@
+require_relative 'invocation'
+
+require 'singleton'
+
+class Quality_of_Service_Observer
+
+	include Singleton
+
+  def initialize
+    @times = {}
+  end
+
+  def set_start(invocation)
+    #usado do lado do cliente
+    #marca o tempo de início de determinada solicitacao
+
+    invocation[:qos_start_time] = Time.now
+
+  end
+
+  def invocation_received (invocation)
+    #usado do lado do servidor
+    #pega o tempo de início de uma requisicao e calcula quanto tempo demorou pra chegar
+    #avalia a qualidade da rede
+
+    received_time = invocation.[:qos_start_time] # recebo a hora que o invocation foi criado -- manipular isso
+
+    @times[:qos_start_time] = received_time
+  end
+
+  def average_time
+    times = []
+    @times.each do |id, t|
+      times << "#{t}"
+    end
+    puts "Max: #{times.max}\nMin: #{times.min}\nMedia: #{(times.max + times.min) / 2}"
+  end
+end
