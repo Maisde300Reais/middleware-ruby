3bc03bdd2b79658233ef1a5b23357b929f726689
luizrogeriocn <luizrogeriocn@gmail.com>
2013-06-17 01:38:31 -0300

    melhorando testes

--- app_gae_snackbar/snackbar_gae_proxy.rb
+++ app_gae_snackbar/snackbar_gae_proxy.rb
@@ -7,26 +7,49 @@ class SnackBarProxy
     @r = Requestor.instance
   end
 
-  def add_food(food)
+  def add_food(food, quantity)
 
     params={} 
 
     params[:http_action] = "post"
-    params[:name]= food.name
-    params[:qtd]= food.qtd
+    params[:request] = "add"
+    params[:food_name] = food.name
+    params[:quantity] = quantity
 
     p @r.invoke("roger-pd-app", "snackbar", params)
   end
 
-  def sell_food(book)
+  def sell_food(food, quantity)
 
     params={} 
 
     params[:http_action] = "post"
     params[:request] = "buy"
-    params[:book] = food.name
+    params[:food_name] = food.name
+    params[:quantity] = quantity
 
     p @r.invoke("roger-pd-app", "snackbar", params)
   end
 
+  def delete_all()
+    params={} 
+    params[:http_action] = "put"
+    p @r.invoke("roger-pd-app", "snackbar", params)
+  end
+
 end
+
+def teste
+  food = Food.new("Coxinha", 5)
+  food2 = Food.new("Risole", 10)
+  food3 = Food.new("Folheado", 20)
+
+  l = SnackBarProxy.new
+  #l.delete_all()
+  #l.add_food(food, 10)
+
+  l.sell_food(food, 1)
+
+end
+
+teste
