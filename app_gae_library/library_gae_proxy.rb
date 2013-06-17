dd731cc2992838b49d6658d2213b93163db258f9
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-06-16 23:15:27 -0300

    começando gae proxy

--- app_gae_library/library_gae_proxy.rb
+++ app_gae_library/library_gae_proxy.rb
@@ -1,1 +1,71 @@
-mainlib.rb
+require_relative '../basic_remote/requestor'
+require_relative 'library'
+require_relative 'client'
+require_relative 'book'
+
+class LibraryProxy
+
+  def initialize
+    @r = Requestor.instance
+  end
+
+  #cada método deve simplesmente traduzir de chamadas normais para chamadas
+  #do requestor
+
+  def add_book(book)
+
+    params={} 
+
+    params[:book_name] = book.name
+    params[:http_action] = "post"
+
+    p @r.invoke("library", "add_book", params)
+  end
+
+  def add_client(client)
+
+    params={} 
+    
+    params[:client_id] = client.id
+    params[:client_name] = client.name
+    params[:http_action] = "post"
+
+    p @r.invoke("library", "add_client", params)
+  end
+
+  def rent_book(client, book)
+
+    params={} 
+
+    params[:book_id]=book.id
+    params[:client_id]=client.id
+    params[:http_action] = "post"
+
+    p @r.invoke("library", "rent_book", params)
+  end
+
+  def return_book(client, book)
+
+    params={} 
+
+    params[:book_id]=book.id
+    params[:client_id]=client.id
+    params[:http_action] = "post"
+
+    p @r.invoke("library", "return_book", params)
+  end
+
+end
+
+def test
+  book= Book.new("1", "Chapeuzinho Vermelho")
+
+  client= Client.new("1", "Igor")
+
+  lib = LibraryProxy.new
+
+  lib.add_client client
+  lib.add_book book
+  lib.rent_book client, book
+  lib.return_book client, book
+end
